153cd8d65dd4240052f9f79ae3a72e9a0c8fc2ae
sura8662 <123110632+sura8662@users.noreply.github.com>
2023-04-08 13:09:09 -0600

    WA11

---
+++ JS/WA11.js
@@ -0,0 +1,26 @@
+const divWithQuote = document.getElementById("insertQuoteHere");
+
+const getQuote = () => {
+    fetch("https://api.kanye.rest")
+        .then((response) => response.json())
+        .then((data) => (divWithQuote.innerHTML = data.quote));
+}
+
+var i = 0;
+function move() {
+    if (i == 0) {
+        i = 1;
+        var elem = document.getElementById("bar");
+        var width = 1;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                width++;
+                elem.style.width = width + "%";
+            }
+        }
+    }
+}
